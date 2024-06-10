4373ea5be5d374d7704092af6b509d975873637c
ichi-h <85932406+ichi-h@users.noreply.github.com>
2024-06-10 23:17:17 +0900

    feat(go): add user service

---
+++ go/services/user/internal/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "user server is running!")
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/", hello)
+	e.Logger.Fatal(e.Start(":1326"))
+}
