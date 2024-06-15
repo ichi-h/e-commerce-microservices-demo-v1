349815945f21c2fea4a34a72807a391c0318cfc5
ichi-h <85932406+ichi-h@users.noreply.github.com>
2024-06-15 10:59:33 +0900

    feat(node/bff): connect go/product server using connectrpc and stab files

--- node/apps/bff/src/index.ts
+++ node/apps/bff/src/index.ts
@@ -1,10 +1,24 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
+import { createPromiseClient } from "@connectrpc/connect";
+import { createConnectTransport } from "@connectrpc/connect-web";
+import { ProductService } from "@e-commerce/product-connect";
+
+const transport = createConnectTransport({
+  baseUrl: "http://localhost:1324",
+});
+
+const client = createPromiseClient(ProductService, transport);
+
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/', async (c) => {
+  const res = await client.getProducts({
+    limit: 1,
+    offset: 0
+  });
+  return c.text(JSON.stringify(res, null, 2))
 })
 
 const port = 3030
